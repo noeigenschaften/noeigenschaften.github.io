f45f31c9b271f0ccc7d38464e41853a2447f1185
noeigenschaften <36310815+noeigenschaften@users.noreply.github.com>
2018-03-27 17:31:07 +0300

    1

---
+++ hw1/main.js
@@ -0,0 +1,22 @@
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+let el = document.getElementById('quote');
+let i = 0;
+
+function changePLS() {
+    readTextFile("https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json", function (text) {
+        let data = JSON.parse(text);
+        el.textContent = '"' + data[i].quoteText + '"'+ ' - ' + data[i].quoteAuthor;
+    });
+    i++;
+};
